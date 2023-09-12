9100599784267d8ef827a8625093f0144598cfbc
laboratorioesan <laboratorioesan@WS2302347>
2023-09-12 08:45:21 -0500

    Agregar archivos de proyecto.

---
+++ Semana04IntroCSharp/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana04IntroCSharp
+{
+    public class Persona
+    {
+        public int dni { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public bool active { get; set; }
+        public Persona(int dni, string name, string phone, bool active)
+        {
+            this.dni = dni;
+            this.name = name;
+            this.phone = phone;
+            this.active = active;
+        }
+        public Persona()
+        {
+                
+        }
+    }
+}
---
+++ Semana04IntroCSharp/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Semana04IntroCSharp;
+
+Console.WriteLine("Hello, World!");
+
+string curso = "Desarrollo de aplicaciones web";
+int edad = 20;
+double salario = 2000;
+
+var alumno = "Luis Chang";
+var mundial = 2026;
+
+var player = new Persona();
+player.dni = 11223344;
+player.name = "Paolo Guerrero";
+player.phone = "051-897787878";
+player.active = true;
+
+var player2 = new Persona()
+{
+    dni = 11335577,
+    name = "Claudio Pizarro",
+    phone = "084-5454545",
+    active = false,
+};
+
+var player3 = new Persona(22446688,"Gianluca Lapadula","878-54578878",true);
+var player4 = new Persona(33559988, "Pedro Gallese", "051-78878787", true);
+
+var personas = new List<Persona>();
+personas.Add(player);
+personas.Add(player2);
+personas.Add(player3);
+personas.Add(player4);
+
+//Buscar a la persona Lapadula con DNI 22446688
+//Linq Query Expressions
+
+var busqueda =  (from p in personas
+                 where p.dni == 22446688
+                 select p).FirstOrDefault();
+
+Console.WriteLine("La persona encontrada es: " 
+                    + busqueda.name);
+
+//Buscar por lambda expressions
+
+var otraBusqueda = personas
+                    .Where(a=>a.dni == 22446688)
+                    .FirstOrDefault();
+
